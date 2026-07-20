c2df9327485f1d691a216c7e017af4ebbe63f45b
Zawar Mehmood <zawar92@gmail.com>
2026-07-20 01:07:30 -0400

    feat: update light button background opacity and wrap in ComponentNoSplashTheme

--- lib/component_light_button.dart
+++ lib/component_light_button.dart
@@ -19,31 +19,50 @@ class ComponentLightButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //  style: ButtonStyle(
+    //       backgroundColor: WidgetStateProperty.all(const Color.fromRGBO(245, 245, 245, 1.0)),
+    //       foregroundColor: WidgetStateProperty.all(Colors.black),
+    //       fixedSize: WidgetStatePropertyAll(Size(double.maxFinite, context.isMobile ? 52 : 54)),
+    //       textStyle: WidgetStatePropertyAll(
+    //         TextStyle(
+    //           fontSize: context.isMobile ? 16 : 19,
+    //           fontWeight: FontWeight.w600,
+    //           fontFamily: fontFamily,
+    //         ),
+    //       ),
+    //       shape: WidgetStateProperty.all(
+    //         RoundedSuperellipseBorder(borderRadius: AppDecoration.borderRadiusStadium),
+    //       ),
+    //       splashFactory: NoSplash.splashFactory,
+    //     ),
     Color defaultBackgroundColor =
         backgroundColor ?? (context.isLightMode ? Colors.grey.shade100 : Colors.grey.shade900);
 
     Color modalSheetBackgroundColor =
-        backgroundColor ?? (context.isLightMode ? Colors.grey.shade300 : Color(0xff2c2c2e));
+        backgroundColor ??
+        (context.isLightMode ? Colors.grey.shade300.withValues(alpha: 0.8) : Color(0xff2c2c2e));
 
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: isModalSheet ? modalSheetBackgroundColor : defaultBackgroundColor,
-        foregroundColor: foregroundColor ?? context.primary,
-        fixedSize: Size(double.maxFinite, context.isMobile ? 52 : 54),
-        textStyle: TextStyle(
-          fontSize: context.isMobile ? 16 : 19,
-          fontWeight: FontWeight.w600,
-          fontFamily: context.textTheme.bodySmall!.fontFamily,
+    return ComponentNoSplashTheme(
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isModalSheet ? modalSheetBackgroundColor : defaultBackgroundColor,
+          foregroundColor: foregroundColor ?? context.primary,
+          fixedSize: Size(double.maxFinite, context.isMobile ? 52 : 54),
+          textStyle: TextStyle(
+            fontSize: context.isMobile ? 16 : 19,
+            fontWeight: FontWeight.w600,
+            fontFamily: context.textTheme.bodySmall!.fontFamily,
+          ),
+          shape: RoundedSuperellipseBorder(
+            borderRadius: AppDecoration.borderRadiusStadium,
+          ),
+          splashFactory: NoSplash.splashFactory,
+          elevation: 0,
+          enabledMouseCursor: SystemMouseCursors.click,
         ),
-        shape: RoundedSuperellipseBorder(
-          borderRadius: AppDecoration.borderRadiusStadium,
-        ),
-        splashFactory: NoSplash.splashFactory,
-        elevation: 0,
-        enabledMouseCursor: SystemMouseCursors.click,
+        child: child,
       ),
-      child: child,
     );
   }
 }
